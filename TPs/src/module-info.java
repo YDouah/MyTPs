69a31bde616d77780958204a537bfe27e6b85e51
YDouah <95980065+YDouah@users.noreply.github.com>
2022-02-13 10:40:30 +0400

    Commnit

--- TPs/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module yu {
-	exports ma.education.tp2.statickeyword;
-	exports ma.education.tp5.collections;
-	exports ma.education.tp6.annotations;
-	exports ma.education.tp2.reflection;
-	exports ma.education.tp2.reflection.rappel;
-	exports ma.education.tp4.singleton;
-}
