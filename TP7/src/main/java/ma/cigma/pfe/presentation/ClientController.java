1106ac56a9ebefa3e1163a0311394dbd614d99df
Youssef DOUAH <toubkal1990@gmail.com>
2022-02-28 12:38:17 -0800

    Tp7

--- TP7/src/main/java/ma/cigma/pfe/presentation/ClientController.java
+++ TP7/src/main/java/ma/cigma/pfe/presentation/ClientController.java
@@ -1,6 +1,7 @@
 package ma.cigma.pfe.presentation;
 
 import ma.cigma.pfe.models.Client;
+import ma.cigma.pfe.service.IClientService;
 
 public class ClientController {
     public void save(Client clt) {
