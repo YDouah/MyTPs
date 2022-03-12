533945972ddf8dc8a0328a9433f541c15dc25ea7
Youssef DOUAH <toubkal1990@gmail.com>
2022-03-12 16:52:52 +0400

    TP9

--- TP10/src/main/java/ma/cigma/pfe/models/ClientNormal.java
+++ TP10/src/main/java/ma/cigma/pfe/models/ClientNormal.java
@@ -7,7 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "T_CLIENT_NORMAL")
+@Table(name = "T-CLIENT-NORMAL")
 @Data
 @NoArgsConstructor
 public class ClientNormal extends Client {
