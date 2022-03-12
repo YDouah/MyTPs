d588771b6ccf88fa86083a9a732053bdc07977f5
Youssef DOUAH <toubkal1990@gmail.com>
2022-03-12 22:22:04 +0400

    TP13

--- Front-End/src/main/java/ma/cigma/FrontApplication.java
+++ Front-End/src/main/java/ma/cigma/FrontApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package ma.cigma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class FrontEndApplication {
+public class FrontApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FrontEndApplication.class, args);
+		SpringApplication.run(FrontApplication.class, args);
 	}
 	@Bean
 	public RestTemplate getRestTemplate() {
