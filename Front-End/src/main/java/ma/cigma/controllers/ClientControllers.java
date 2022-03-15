e0c3745d2d9de3af2dc94aba33c9d6858ac82cda
Youssef DOUAH <toubkal1990@gmail.com>
2022-03-15 15:32:06 +0400

    TP13

--- Front-End/src/main/java/ma/cigma/controllers/ClientControllers.java
+++ Front-End/src/main/java/ma/cigma/controllers/ClientControllers.java
@@ -3,6 +3,7 @@ package ma.cigma.controllers;
 import ma.cigma.models.Client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -12,6 +13,7 @@ import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
+@Controller
 public class ClientControllers {
 
     @Value("${api.url}")
@@ -21,23 +23,22 @@ public class ClientControllers {
     private RestTemplate restTemplate;
 
     @GetMapping(value = {"/", "/clients"})
-    public String home(Model model) {model.addAttribute("client",new Client());
+    public String home(Model model) {
+        model.addAttribute("client",new Client());
         List<Client> clients = restTemplate.getForObject(apiUrl+"/clients/all", List.class);
         model.addAttribute("clients",clients);
         return "index-client";
     }
 
-    @PostMapping(value = "/add-client")
-    public String addClient(Model model, @ModelAttribute("client") Client client) {restTemplate.postForObject(apiUrl+"/clients/add",
-                client,
-                Client.class);
-        return "redirect:/clients";
-    }
-    @GetMapping(value = {"/delete-client/{id}"})
-    public String deleteClientById(
-            Model model, @PathVariable long id) {
-        restTemplate.delete(apiUrl+"/clients/"+id);
+    @PostMapping(value = {"/add-client"})
+    public String addClient(Model model, @ModelAttribute Client client) {
+        restTemplate.postForObject(apiUrl + "/clients", client, Client.class);
         return "redirect:/clients";
     }
 
+    @GetMapping(value = {"/delete-client/{id}"})
+    public String deleteClientById(Model model, @PathVariable long id) {
+        restTemplate.delete(apiUrl + "/clients/" + id);
+        return "redirect:/clients";
+    }
 }
